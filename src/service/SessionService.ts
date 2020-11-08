9b9288a4a9dd0a08fc17981b170904488e7ad040
Иван Поддубный <ip@webpractik.ru>
2020-11-08 22:53:35 +0300

    fix(ds): неправильный формат файла

--- src/service/SessionService.ts
+++ src/service/SessionService.ts
@@ -81,13 +81,20 @@ class SessionService {
         try {
             this.saveFile(data);
             if (this.demoMode) {
-                //@ts-ignore
-                let server = new DemoMoveFileService(data);
-                server.moveAction();
+                setTimeout(() => {
+                    //@ts-ignore
+                    let server = new DemoMoveFileService(data);
+                    server.moveAction();
+                }, SessionService.randomInteger(5, 90));
             }
         } catch (e) {
             logger.error('DEMO-режим - файл ' + data.fileId + ' не был сохранен с ошибкой' + e.message);
         }
+    }
+
+    private static randomInteger(min: number, max: number) {
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+        return Math.round(rand);
     }
 
     subscribe() {
@@ -128,8 +135,6 @@ class SessionService {
 
             // SERVER SENT FILE
             .on('session-file-available', (data: any) => {
-                logger.info(`Стал доступен файл ${data.fileId} в сессии `);
-
                 if (this.demoMode) {
                     this.saveFileInDemoMode(data);
                 } else {
