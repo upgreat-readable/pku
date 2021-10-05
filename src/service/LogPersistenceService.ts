3a1fd7bd070d258aa7a4d57fc0a593f5b9da4e8a
Pjotr Shkumat <p.shkumat@webpractik.ru>
2021-10-05 14:39:57 +0300

    refactor(logs): вынес получение всех записей лога в отдельный метод
    Refs: z9vm07, z45gp6

--- src/service/LogPersistenceService.ts
+++ src/service/LogPersistenceService.ts
@@ -5,29 +5,30 @@ import { logPersistencePath } from '../config';
 
 class LogPersistenceService {
     public async getEntries(from: Date): Promise<Array<LogEntry>> {
+        const logEntries = await LogPersistenceService.getLogEntries();
+
+        return logEntries.filter(entry => new Date(entry.timestamp) > from);
+    }
+
+    public async trimLog(to: Date): Promise<void> {
+        const logEntries = await LogPersistenceService.getLogEntries();
+
+        const dataToKeep = logEntries
+            .filter(entry => new Date(entry.timestamp) > to)
+            .map(entry => JSON.stringify(entry))
+            .join('\n');
+
+        fs.writeFileSync(logPersistencePath, dataToKeep);
+    }
+
+    private static async getLogEntries(): Promise<Array<LogEntry>> {
         return fs
             .readFileSync(logPersistencePath)
             .toString()
             .split('\n')
             .filter(Boolean)
             .map(LogPersistenceService.parseLine)
-            .filter(entry => entry.timestamp)
-            .filter(entry => new Date(entry.timestamp) > from);
-    }
-
-    public async trimLog(to: Date): Promise<void> {
-        const dataToKeep = fs
-            .readFileSync(logPersistencePath)
-            .toString()
-            .split('\n')
-            .filter(Boolean)
-            .map(LogPersistenceService.parseLine)
-            .filter(entry => entry.timestamp)
-            .filter(entry => new Date(entry.timestamp) > to)
-            .map(entry => JSON.stringify(entry))
-            .join('\n');
-
-        fs.writeFileSync(logPersistencePath, dataToKeep);
+            .filter(entry => entry.timestamp);
     }
 
     private static parseLine(line: string): any {
