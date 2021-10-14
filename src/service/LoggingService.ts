4a9fed0c94213e5c3aba78e079a33958fb243d98
Pjotr Shkumat <p.shkumat@webpractik.ru>
2021-10-14 13:54:02 +0300

    refactor(logs): убрал неиспользуемый код
    Refs: z9vm07, z45gp6

--- src/service/LoggingService.ts
+++ src/service/LoggingService.ts
@@ -1,14 +1,6 @@
 import { LogEntry, Logger } from 'winston';
 
-import SIClient, { SocketIoClient } from '../connections/SocketIoClient';
-
 class LoggingService {
-    client: SocketIoClient;
-
-    constructor() {
-        this.client = SIClient;
-    }
-
     process(logger: Logger, info: LogEntry) {
         logger.log(info);
     }
